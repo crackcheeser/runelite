b7bb71669e80648223b4000af65d1f60099def35
crackcheeser <kaascracker@gmail.com>
2026-01-01 16:54:34 +0100

    Align gorilla IDs and hitsplat handling with current API

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/demonicgorilla/DemonicGorillaConfig.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2024, RuneLite
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.runelite.client.plugins.demonicgorilla;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+
+@ConfigGroup("demonicgorilla")
+public interface DemonicGorillaConfig extends Config
+{
+    @ConfigItem(
+        keyName = "showOverlay",
+        name = "Show overlay",
+        description = "Display possible attack styles above demonic gorillas"
+    )
+    default boolean showOverlay()
+    {
+        return true;
+    }
+
+    @ConfigItem(
+        keyName = "hideWhenNotTargeted",
+        name = "Hide when not targeted",
+        description = "Only show overlay when the gorilla is targeting a player"
+    )
+    default boolean hideWhenNotTargeted()
+    {
+        return false;
+    }
+}
---
+++ runelite-client/src/main/java/net/runelite/client/plugins/demonicgorilla/DemonicGorillaProjectileIds.java
@@ -0,0 +1,16 @@
+/*
+ * Placeholder projectile IDs for demonic gorillas.
+ * TODO: Replace these values with real projectile IDs from the current client when available.
+ */
+package net.runelite.client.plugins.demonicgorilla;
+
+final class DemonicGorillaProjectileIds
+{
+    private DemonicGorillaProjectileIds()
+    {
+    }
+
+    static final int MAGIC = -1;   // TODO: fill with actual magic projectile id
+    static final int RANGED = -2;  // TODO: fill with actual ranged projectile id
+    static final int BOULDER = -3; // TODO: fill with actual boulder projectile id
+}
